92b20bc44d2a3043b8091ba8de8ace3acc9e4fa1
EthanSpearey <1302228@spsd.sk.ca>
2024-09-11 12:15:28 -0600

    Finished Coordinate Systems

--- 01 Basics of Drawing/sketch.js
+++ 01 Basics of Drawing/sketch.js
@@ -1,15 +1,28 @@
-// Project Title
-// Your Name
-// Date
+// Basics of Drawing
+// Ethan Spearey
+// 2024-09-11
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// 2D primitives, colour, animation, mouse/keyboard events
 
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+function setup()
+{
+  createCanvas(500, 400);
 }
 
-function draw() {
+function draw()
+{
   background(220);
+  drawCircles();
+}
+
+function drawCircles()
+{
+  fill(101, 0, 255);
+  circle(0, 0, 50);
+  circle(width/2, height/2, 50);
+  circle(width, 0, 50);
+  circle(width, height, 50);
+  circle(0, height, 50);
+  circle(width*(2/3), height/2, 50)
 }
