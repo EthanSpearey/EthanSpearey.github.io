a0dd82b887c407b68dacedb00d6280916d5a3cc7
EthanSpearey <1302228@spsd.sk.ca>
2024-09-11 11:26:54 -0600

    Started Demo 01

---
+++ 00 First p5 Project/sketch.js
@@ -0,0 +1,14 @@
+// First Day Demo
+// Ethan Spearey
+// 2024-09-10
+
+// Interactive Programs -> don't finish...
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  //background(220);
+  circle(mouseX, mouseY, 20);
+}
