32db31139b08d0c860373a44b6b012d06fe08c45
EthanSpearey <1302228@spsd.sk.ca>
2024-10-02 12:14:07 -0600

    Day of Confusion

---
+++ 08 Perlin Noise Project/sketch.js
@@ -0,0 +1,18 @@
+// Perlin Noise Project
+// Ethan Spearey
+// 2024-10-02
+
+function setup()
+{
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw()
+{
+  background(220);
+}
+
+function generateTerrain()
+{
+  
+}
