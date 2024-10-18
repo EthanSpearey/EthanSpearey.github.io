a382924e2297bb845fbb8824aec8dc83b9898abb
EthanSpearey <1302228@spsd.sk.ca>
2024-10-18 12:15:38 -0600

    CAAAAAAAAAAAAARS!

---
+++ 18 CARS! CARS! CARS!/sketch.js
@@ -0,0 +1,18 @@
+// CARS! CARS! CARS!
+// Ethan Spearey
+// 2024-10-18
+
+function setup()
+{
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw()
+{
+  background(220);
+  
+  //Road
+  fill(20);
+  rect(0, windowHeight/4, windowWidth, windowHeight/2);
+  
+}
